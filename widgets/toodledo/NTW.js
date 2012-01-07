8bd5b0d13832fde0728c34a955cbc047a6d6cb60
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2012-01-06 17:36:16 -0800

    Added Toodledo widget

---
+++ widgets/toodledo/NTW.js
@@ -0,0 +1,27 @@
+var NTWID = 'kglfkhnhcnemodbfakimgonfkbpmlpfd';
+
+function registerWidget(info) {
+    chrome.extension.onRequestExternal.addListener(function(request, sender, sendResponse) {
+        var parts = request.split('-');
+        if(parts[0] == 'NewTabWidgets') {
+            info['head'] = request + "back";
+            chrome.extension.sendRequest(NTWID, info);
+        }
+    });
+}
+
+function setTitle(title) {
+    var data = {
+        head: 'NewTabWidgets-setTitle',
+        content: title
+    }
+    chrome.extension.sendRequest(NTWID, data);
+}
+
+function setHeight(height) {
+    var data = {
+        head: 'NewTabWidgets-setHeight',
+        content: height
+    }
+    chrome.extension.sendRequest(NTWID, data);
+}
