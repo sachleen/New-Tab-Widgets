b854e5e90fe2b5c67b5ecf2d1ccd8a23d7ab6fd2
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2012-01-08 15:06:06 -0800

    Updated NTW.js file so it no longer requires NTW's ID. All requests are sent back to the sender. This allows for a test and production instance of NTW without having duplicates of the widgets.

--- widgets/clock/NTW.js
+++ widgets/clock/NTW.js
@@ -1,11 +1,9 @@
-var NTWID = 'kglfkhnhcnemodbfakimgonfkbpmlpfd';
-
 function registerWidget(info) {
     chrome.extension.onRequestExternal.addListener(function(request, sender, sendResponse) {
         var parts = request.split('-');
         if(parts[0] == 'NewTabWidgets') {
             info['head'] = request + "back";
-            chrome.extension.sendRequest(NTWID, info);
+            chrome.extension.sendRequest(sender.id, info);
         }
     });
 }
